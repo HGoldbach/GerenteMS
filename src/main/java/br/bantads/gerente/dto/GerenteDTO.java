2c51bc4ee671926b542aacea0c26d10124ba2549
Goldbach <goldbachcs@gmail.com>
2023-06-25 21:01:22 -0300

    Remoção Lombok

--- src/main/java/br/bantads/gerente/dto/GerenteDTO.java
+++ src/main/java/br/bantads/gerente/dto/GerenteDTO.java
@@ -6,9 +6,7 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class GerenteDTO implements Serializable {
     private Integer id;
     private String nome;
@@ -17,4 +15,65 @@ public class GerenteDTO implements Serializable {
     private String cpf;
     private String perfil;
 
+    public GerenteDTO() {
+        super();
+    }
+
+    public GerenteDTO(Integer id, String nome, String email, String telefone, String cpf, String perfil) {
+        super();
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.cpf = cpf;
+        this.perfil = perfil;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
 }
--- src/main/java/br/bantads/gerente/model/Gerente.java
+++ src/main/java/br/bantads/gerente/model/Gerente.java
@@ -8,9 +8,6 @@ import lombok.NoArgsConstructor;
 import java.io.Serializable;
 import java.util.UUID;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "tb_gerentes")
 public class Gerente implements Serializable, Comparable<Gerente> {
@@ -28,6 +25,69 @@ public class Gerente implements Serializable, Comparable<Gerente> {
     private String telefone;
     @Column(name="perfil")
     private String perfil;
+
+    public Gerente() {
+        super();
+    }
+
+    public Gerente(Integer id, String nome, String email, String cpf, String telefone, String perfil) {
+        super();
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.perfil = perfil;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+
     @Override
     public int compareTo(Gerente gerente) {
         return getNome().compareTo(gerente.getNome());
